2d1c33126949ae86ac76228da8c8c40b834b4408
Muballahgul <gul_lisha@yahoo.com>
2020-10-13 19:00:40 -0400

    Added additional Assertion.

--- src/test/java/test/Test1.java
+++ src/test/java/test/Test1.java
@@ -1,9 +1,12 @@
 package test;
 
+import org.testng.Assert;
+
 public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("I am adding new test");
+		Assert.assertEquals(20, 20);
 
 	}
 
